e97de35511000d15a110b923dc357acf344b2439
Dara Ladje <daraladje@gmail.com>
2019-05-05 14:28:44 -0400

    Initial Commit

---
+++ yard/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  yard
+//
+//  Created by Dara Ladjevardian on 5/5/19.
+//  Copyright © 2019 Dara Ladjevardian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
